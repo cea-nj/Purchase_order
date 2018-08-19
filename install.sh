84f75cc53ae8db1691dee56b611b1395f49d1681
cea-nj <42520249+cea-nj@users.noreply.github.com>
2018-08-19 14:42:08 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,38 @@
-\curl -L https://get.rvm.io | bash -s stable --rails
+#!/bin/bash
+
+curl -L https://get.rvm.io | bash -s stable --rails
 gem install pg
 su - postgres
 create role myapp with createdb login password 'password1';
 rails new myapp --database=postgresql
-development:
+
+cat << EOF>> myapp/config/database.yml
+
+default: &default
   adapter: postgresql
   encoding: unicode
-  database: myapp_development
-  pool: 5
+  # For details on connection pooling, see Rails configuration guide
+  # http://guides.rubyonrails.org/configuring.html#database-pooling
+  pool: <%= ENV.fetch("RAILS_MAX_THREADS") { 5 } %>
   username: myapp
   password: password1
+
+development:
+  <<: *default
+  database: myapp_development
   
-  rake db:setup
+  test:
+  <<: *default
+  database: myapp_test
   
-  rails generate scaffold Purchase_Order item_description:string item_no:string qty:string units:string price:string total:string dept:string po_Number:string cardholders_Name:string vendor:string vendor_address:string requested_By:string purpose_Of_Purchase:string date:timestamp
+  production:
+  <<: *default
+  database: myapp_production
+  username: myapp
+  password: <%= ENV['MYAPP_DATABASE_PASSWORD'] %>
+EOF
+  
+rake db:setup  
+rails generate scaffold Purchase_Order item_description:string item_no:string qty:string units:string price:string total:string dept:string po_Number:string cardholders_Name:string vendor:string vendor_address:string requested_By:string purpose_Of_Purchase:string date:timestamp
 rake db:migrate
 rails server
